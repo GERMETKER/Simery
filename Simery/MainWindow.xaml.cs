11ef3fdbfe091933c66d0b93f0ae1b00213904ad
Top <Top@WIN-R1H7VOA209G>
2025-10-07 20:42:57 +0500

    d

--- Simery/MainWindow.xaml.cs
+++ Simery/MainWindow.xaml.cs
@@ -16,9 +16,54 @@ namespace Simery
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<ToDo> CasesList;
+
         public MainWindow()
         {
+            CasesList = new List<ToDo>();
+
+            /*foreach (var i in CasesList)
+            {
+                ListToDo.Items.Add(i);
+
+            }*/
+
+
+
+            CasesList.Add(new ToDo("Приготовить покушать", new DateTime(2024, 1, 15), "Нет описания"));
+            CasesList.Add(new ToDo("Поработать", new DateTime(2024, 1, 20), "Съездить на совещание в Москву"));
+            CasesList.Add(new ToDo("Отдохнуть", new DateTime(2024, 2, 1), "Съездить в отпуск в Сочи"));
+
+            ListToDo.ItemsSource = CasesList;
+
+
+
+
             InitializeComponent();
+
+           
+
         }
+
+        private void AddCase(object sender, RoutedEventArgs e)
+        {
+            string case_name = titleToDo.Text;
+            DateTime? date_case = dateToDo.SelectedDate;
+            string case_description = descriptionToDo.Text;
+
+            CasesList.Add(new ToDo(case_name, date_case, case_description));
+
+            
+        }
+
+        private void Box_Checked(object sender, RoutedEventArgs e)
+        {
+            groupBoxToDo.Visibility = Visibility.Visible;
+        }
+        private void Box_Unchecked(object sender, RoutedEventArgs e)
+        {
+            groupBoxToDo.Visibility = Visibility.Hidden;
+        }
+
     }
 }
---
+++ Simery/ToDo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simery
+{
+    public class ToDo
+    {
+
+        public string CaseName;
+        public DateTime? TimeOfCompleating;
+        public string Description;
+
+        
+
+        public ToDo(string caseName, DateTime? timeOfCompleating, string description)
+        {
+            CaseName = caseName;
+            TimeOfCompleating = timeOfCompleating;
+            Description = description;
+        }
+
+    }
+}
