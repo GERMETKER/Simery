5a2de0078d4fb57757599dfd537153e6250974f5
Top <Top@WIN-R1H7VOA209G>
2025-10-10 20:37:31 +0500

    g

--- Simery/MainWindow.xaml.cs
+++ Simery/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Simery
 {
@@ -22,38 +23,47 @@ namespace Simery
         {
             CasesList = new List<ToDo>();
 
-            /*foreach (var i in CasesList)
-            {
-                ListToDo.Items.Add(i);
-
-            }*/
 
 
+            InitializeComponent();
 
             CasesList.Add(new ToDo("Приготовить покушать", new DateTime(2024, 1, 15), "Нет описания"));
             CasesList.Add(new ToDo("Поработать", new DateTime(2024, 1, 20), "Съездить на совещание в Москву"));
             CasesList.Add(new ToDo("Отдохнуть", new DateTime(2024, 2, 1), "Съездить в отпуск в Сочи"));
 
+
+            ListToDo.ItemsSource = CasesList;
+
             
 
 
-
-
-            InitializeComponent();
-
-           
 
         }
 
         private void AddCase(object sender, RoutedEventArgs e)
         {
+
+            ListToDo.ItemsSource = CasesList;
             string case_name = titleToDo.Text;
             DateTime? date_case = dateToDo.SelectedDate;
             string case_description = descriptionToDo.Text;
 
+
+
             CasesList.Add(new ToDo(case_name, date_case, case_description));
 
-            
+            titleToDo.Text = null;
+            dateToDo.SelectedDate = null;
+            descriptionToDo.Text = null;
+
+            UpdateList();
+
+        }
+
+        private void UpdateList()
+        {
+            ListToDo.ItemsSource = null;
+            ListToDo.ItemsSource = CasesList;
         }
 
         private void Box_Checked(object sender, RoutedEventArgs e)
@@ -65,5 +75,10 @@ namespace Simery
             groupBoxToDo.Visibility = Visibility.Hidden;
         }
 
+        private void DelCase(object sender, RoutedEventArgs e)
+        {
+            CasesList.Remove(ListToDo.SelectedItem as ToDo);
+            UpdateList();
+        }
     }
 }
