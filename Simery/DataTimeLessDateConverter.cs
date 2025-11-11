02677c3257c523bfd5ac1ed6f9e0022ed72c958e
GERMETKER <160734087+GERMETKER@users.noreply.github.com>
2025-11-11 19:58:41 +0500

    Create DataTimeLessDateConverter.cs

---
+++ Simery/DataTimeLessDateConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Simery
+{
+    public class DataTimeLessDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var d = value as DateTime?;
+            if(d != null)
+            {
+                return DateTime.Now.Date > d.Value.Date;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
